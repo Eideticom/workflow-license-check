5a59471958ea92939884dc25b38a7564ff1ffd73
Wade Lockhart <wade.lockhart@eideticom.com>
2022-08-26 15:28:07 -0600

    spdx_review: Add file logging argument and default
    Import the logging package for file logging of stdout messages.
    The level is defaulted to WARNING, but an optional cmdline argument
    --log/-l can be used to set it to a supported python log level.
    
    The logging messages are written to spdx_review.log file.

--- spdx_review.py
+++ spdx_review.py
@@ -8,8 +8,8 @@ import json
 import subprocess
 from pathlib import Path
 import tempfile
+import logging
 import sys
-
 
 def mk_copyright_list(dic):
     copyright_list = []
@@ -44,25 +44,33 @@ def report_differences(old_dict, new_dict):
     for fname, new in new_dict.items():
         if fname not in old_dict:
             print(f"INFO: New file: {fname} detected. Checking now for license expressions and copyrights")
+            logging.info(f"INFO: New file: {fname} detected. Checking now for license expressions and copyrights")
             if not new['license_expressions']:
                 print(f"WARN: New file {fname} is showing no license_expressions: {new['license_expressions']}")
+                logging.warning(f"New file {fname} is showing no license_expressions: {new['license_expressions']}")
             else:
                 print(f"INFO: New file {fname} is showing license_expressions: {new['license_expressions']}")
+                logging.info(f"INFO: New file {fname} is showing license_expressions: {new['license_expressions']}")
             if not new['copyrights']:
                 print(f"ERROR: New file {fname} is showing no copyrights: {new['copyrights']}")
                 error_flag = True
+                logging.error(f"New file {fname} is showing no copyrights: {new['copyrights']}")
             else:
                 print(f"INFO: New file {fname} is showing copyrights: {new['copyrights']}")
+                logging.info(f"INFO: New file {fname} is showing copyrights: {new['copyrights']}")
             continue
         old = old_dict[fname]
         if new['copyrights'] != old['copyrights']:
             if not new['copyrights']:
                 print(f"ERROR: {fname} is showing no copyrights: {new['copyrights']}")
                 error_flag = True
+                logging.error(f"{fname} is showing no copyrights: {new['copyrights']}")
             else:
                 print(f"WARN: {fname} Copyright has changed from {old['copyrights']} to {new['copyrights']}")
+                logging.warning(f"{fname} Copyright has changed from {old['copyrights']} to {new['copyrights']}")
         if new['license_expressions'] != old['license_expressions']:
             print(f"WARN: {fname} License Expression has changed from {old['license_expressions']} to {new['license_expressions']}")
+            logging.warning(f"{fname} License Expression has changed from {old['license_expressions']} to {new['license_expressions']}")
     return error_flag
 
 def run_scancode(directory):
@@ -86,9 +94,14 @@ if __name__ == "__main__":
     group.add_argument("-j", "--jsons", action="store_true")
     group.add_argument("-g", "--git", action="store_true")
     parser.add_argument("-G", "--git_dir", type=Path)
+    parser.add_argument("-l", "--log", default="WARN", choices=["INFO", "WARN", "ERROR"])
     parser.add_argument("before")
     parser.add_argument("after")
     args = parser.parse_args()
+
+    numeric_level = getattr(logging, args.log.upper(), None)
+    logging.basicConfig(filename='spdx_review.log', format='%(levelname)s - %(message)s',
+                        level=numeric_level)
 
     try:
         if args.dirs:
